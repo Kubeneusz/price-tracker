b081c6224a0525bc24ed7d3e38c430eff7574702
Kubeneusz <67506555+Kubeneusz@users.noreply.github.com>
2026-03-30 19:57:18 +0200

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,10 @@
+import requests
+
+def notify(msg):
+    TOKEN = "TWÓJ_TOKEN"
+    CHAT_ID = "TWÓJ_CHAT_ID"
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
